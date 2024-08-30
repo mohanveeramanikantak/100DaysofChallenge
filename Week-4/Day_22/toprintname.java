021b044608bcbb1eec71ebe3e81b5bd5ee205f95
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-08-30 21:55:23 +0530

    Add files via upload

---
+++ Week-4/Day_22/toprintname.java
@@ -0,0 +1,9 @@
+//Basic Java program
+class toprintname
+{
+	public static void main(String args[])
+	{
+		String name = "Hello EveryOne , This is My First Java Program";
+		System.out.println(name);
+	}
+}

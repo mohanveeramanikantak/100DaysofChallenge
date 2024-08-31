3ad9feaefeac0c7c71fc3178e7d49176f4fb570a
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-08-31 21:30:44 +0530

    Add files via upload

---
+++ Week-4/Day_23/floatkey.java
@@ -0,0 +1,13 @@
+//Using Float Keyword
+class floatkey
+{
+	public static void main(String args[])
+	{
+		float a = 5.5f;
+		float b = 5.5f;
+		float c = a + b;
+		System.out.println(" We Entered That A Value is " +a);
+		System.out.println(" We Entered That B Value is " +b);
+		System.out.println(" Addition of Floating Two Values is" +c);
+	}
+}
---
+++ Week-4/Day_23/intkey.java
@@ -0,0 +1,13 @@
+// Using Int Keyword
+class intkey
+{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("We Entered that a Value is " + a);
+        System.out.println("We Entered that b Value is " + b);
+        System.out.println("Addition of A + B is " + c);
+    }
+}

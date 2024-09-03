f705228b26708fb78fb77ef978f42532bc1b948d
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-03 21:36:32 +0530

    Add files via upload

---
+++ Week-4/Day_25/BooleanExample.java
@@ -0,0 +1,9 @@
+// using Boolean KeyWord
+public class BooleanExample 
+{
+    	public static void main(String[] args) 
+	{
+        	boolean myBool = true;
+        	System.out.println("The value of myBool is: " + myBool);
+    	}
+}
---
+++ Week-4/Day_25/CharExample.java
@@ -0,0 +1,9 @@
+//using char KeyWord
+public class CharExample 
+{
+    	public static void main(String[] args) 
+	{
+        	char myLetter = 'D';
+        	System.out.println("The value of myLetter is: " + myLetter);
+    	}
+}

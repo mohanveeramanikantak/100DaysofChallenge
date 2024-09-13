b98d027b7e2eff109a499fb275d78714a956fe23
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-13 21:04:28 +0530

    Add files via upload

---
+++ Week-5/Day_35/Methods.java
@@ -0,0 +1,12 @@
+class Methods
+{
+  	static void myMethod() 
+	{
+    		System.out.println("I'm so executed! to learn java Methods ");
+  	}
+
+  	public static void main(String[] args) 
+	{
+    		myMethod();
+  	}
+}

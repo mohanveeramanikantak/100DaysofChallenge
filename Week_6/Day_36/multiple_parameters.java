52e9f6e5bbb75eaf308ce1c316d4031105f1af6a
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-14 19:45:11 +0530

    Add files via upload

---
+++ Week_6/Day_36/multiple_parameters.java
@@ -0,0 +1,13 @@
+class multiple_parameters 
+{
+    	public static void addNumbers(int num1, int num2) 
+	{
+        	int sum = num1 + num2;
+        	System.out.println("Sum: " + sum);
+    	}
+
+    	public static void main(String[] args) 
+	{
+        	addNumbers(5, 10);
+    	}
+}

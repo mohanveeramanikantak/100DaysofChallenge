b088b95db3774387d8d056a60082160a5858907d
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-24 20:17:22 +0530

    Add files via upload

---
+++ Week_6/Day_42/Accessing_attributes.java
@@ -0,0 +1,10 @@
+class Accessing_attributes
+{
+  	int x = 5;
+
+  	public static void main(String[] args) 
+	{
+    		Accessing_attributes myObj = new Accessing_attributes();
+    		System.out.println(myObj.x);
+  	}
+}

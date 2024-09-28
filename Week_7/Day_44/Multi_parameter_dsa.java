ffd1247557420d4b26aa2e98623b76f811734ea9
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-28 20:02:22 +0530

    Add files via upload

---
+++ Week_7/Day_44/Multi_parameter_dsa.java
@@ -0,0 +1,25 @@
+// To Print Constructor in Multi Parameter With Data Scan Assign
+import java.util.Scanner;
+class Self
+{
+	Self(int think, int stop)
+	{
+	}
+}
+class Multi_parameter_dsa
+{
+	public static void main(String args[])
+	{
+		Scanner kia=new Scanner(System.in);
+		System.out.print(" Enter the think Value ");
+		int thinks=kia.nextInt();
+		System.out.print(" Enter the stop Value ");
+		int stops=kia.nextInt();
+		System.out.println(" Think value is : " +thinks);
+		System.out.println(" Stop value is : " +stops);
+	}
+	void Self()
+	{
+		Self s;
+	}
+}

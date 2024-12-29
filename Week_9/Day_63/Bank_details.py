125f4f78de29076288e8ea9f5e4c9555c4b55def
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-12-29 20:41:00 +0530

    Add files via upload

---
+++ Week_9/Day_63/Bank_details.py
@@ -0,0 +1,15 @@
+#To Print a Bank Details
+Name="Kalepu.Mohan.Veera.Manikanta"
+Son_of="Kalepu.Srinivasu"
+Bank="Sbi"
+Branch="Mandapeta"
+Custid="6029800520"
+AC_no="002310089360"
+pho_no="9885149609"
+print("Name is       :",Name)
+print("Son/of is     :",Son_of)
+print("Bank Name is  :",Bank)
+print("Branch is     :",Branch)
+print("Custmer Id is :",Custid)
+print("Account num is:",AC_no)
+print("Phone num is  :",pho_no)

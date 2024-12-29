2786fc6aefc6cb7ed6a489efc486a3778532069e
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-12-29 20:42:23 +0530

    Add files via upload

---
+++ Week_9/Day_63/College_address.py
@@ -0,0 +1,6 @@
+College_name="PR Junior College"
+College_address="Balagi Cheruvu"
+City="Kakinada"
+print(College_name)
+print(College_address)
+print(City)
---
+++ Week_9/Day_63/Employe_details.py
@@ -0,0 +1,13 @@
+# To print Employe Details#
+emp_Name="B.Kumar"
+emp_id="MKS01765"
+emp_Company="MK SOLUTIONS"
+emp_salary="78,000"
+company_address="Software City"
+City="Vizag"
+print("Name is         :",emp_Name)
+print("Id no is        :",emp_id)
+print("Company Name is :",emp_Company)
+print("Salary is       :",emp_salary)
+print("Company adrs is :",company_address)
+print("city is         :",City)

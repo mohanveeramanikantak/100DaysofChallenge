452f12c2a1d9fbfd2039f6de874048525b2480fc
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-10 22:00:18 +0530

    Add files via upload

---
+++ Week_11/Day_72/Append( )_Addind_Elemant_to_a_list.py
@@ -0,0 +1,8 @@
+#animals list
+animals=['Cat','Dog','Rabbit']
+
+#'Guinea pig' is appended to the animals list
+animals.append('Guinea pig')
+
+#updated animals list
+print('updated animals list:',animals)
---
+++ Week_11/Day_72/Append( )_Adding_list_to_a_list.py
@@ -0,0 +1,10 @@
+#animals list
+animals=['cat','dog','rabbit']
+
+#list for wild animals
+wild_animals=['tiger','fox']
+
+#appending Wild_animals list to the animals list
+animals.append(wild_animals)
+
+print('Updated animlas list;l',animals)

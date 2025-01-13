d0869047bd70ab1e07f89691b21e7e5ac3b66cb9
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-13 21:36:50 +0530

    Add files via upload

---
+++ Week_11/Day_75/Insert( )_Return_Value_Form_list.py
@@ -0,0 +1,6 @@
+#Vowel list
+vowel=['a','e','i','u']
+#'O' is inserted at index3
+# the postion of 'O' will be be 4th
+vowel.insert(3,10)
+print('updated list :',vowel)
---
+++ Week_11/Day_75/Insert( )_a_Tuple_of_the_list.py
@@ -0,0 +1,10 @@
+#Inseting a Tuple(as an Element)of the list
+mixed_list=[{1,2},[5,6,7]]
+
+#number tuple
+number_tuple=(3,4)
+
+#insertoing a tuple to the list
+mixed_list.insert(1,number_tuple)
+
+print('updated List:',mixed_list)

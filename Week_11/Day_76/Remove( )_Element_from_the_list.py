a2fb42a46cdc19d04e42a79ba6ef5f935872b71d
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-15 23:49:56 +0530

    Add files via upload

---
+++ Week_11/Day_76/Remove( )_Element_from_the_list.py
@@ -0,0 +1,8 @@
+#Remove element from the list
+#animal list
+animals=['cat','dog','rabbit','guinea pig']
+
+#'rabbit' is removed
+animals.remove('rabbit')
+#updated animlas list
+print('Updated animals list',animals)
---
+++ Week_11/Day_76/Remove( )_Method_ona_having_duplicate_element.py
@@ -0,0 +1,8 @@
+#animals list
+animals=['dog','cat','cat','guniea pig','cat']
+
+#'cat' is removed
+animals.remove('cat')
+
+#Upadated animals list
+print('updated animls list :',animals)

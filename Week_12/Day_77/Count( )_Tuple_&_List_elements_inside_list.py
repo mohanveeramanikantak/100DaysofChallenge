28aaa79105161561e43052705dd2d85cf9ed4532
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-16 21:37:47 +0530

    Add files via upload

---
+++ Week_12/Day_77/Count( )_Tuple_&_List_elements_inside_list.py
@@ -0,0 +1,14 @@
+#Rondam list
+random=['a',('a','b'),('a','b'),[3,4]]
+
+#count element('a','b')
+count=random.count(('a','b'))
+
+#print count
+print("The Count of('a', 'b') is :",count)
+
+#count element[3,4]
+count=random.count([3,4])
+
+#print count
+print('The Count of[3,4] is :',count)
---
+++ Week_12/Day_77/Count( )_using.py
@@ -0,0 +1,15 @@
+#use count()
+#Vowels list
+vowels=['a','e','i','o','i','u']
+
+#count element of 'i'
+count=vowels.count('i')
+
+#print count of i
+print('The Count of i is :',count)
+
+#count element of 'p'
+count=vowels.count('p')
+
+#print count of P
+print('The Count if p is :',count)

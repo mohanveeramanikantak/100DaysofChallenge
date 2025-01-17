c3aa40e266206ca14dc3a684f14da6aebff6d024
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-17 07:45:05 +0530

    Add files via upload

---
+++ Week_12/Day_78/Pop( )_Without_an_inde.py
@@ -0,0 +1,17 @@
+#programming language list
+language=['python','java','c++','ruby','c']
+
+#remove and return the list item
+print('When index is not passed:')
+print('Return value is :',language.pop())
+print('updated list:',language))
+
+#remove and return the list item
+print('\n When -1 is passed:')
+print('Return value :',language.pop(-1))
+print('updated liat:',language)
+
+#remove and return the list items
+print('\n When -3 is passed:')
+print('Return value :',language.pop(-3))
+print('updated liat:',language)
---
+++ Week_12/Day_78/Pop( )_item_at the_given_index_form_the_list.py
@@ -0,0 +1,9 @@
+#programming language list
+language=['python','java','c++','French','c']
+
+#removed and return the 4th item
+return_value=language.pop(3)
+print('Return value :',return_value)
+
+#updated the list
+print('Updated list:',language)

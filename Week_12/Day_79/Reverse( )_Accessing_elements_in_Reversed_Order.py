1cd9c069b2c9f2986e307ca9d15388baa72121a6
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-18 19:15:28 +0530

    Add files via upload

---
+++ Week_12/Day_79/Reverse( )_Accessing_elements_in_Reversed_Order.py
@@ -0,0 +1,6 @@
+#operting System list
+system=['Mankanta','Loves','Harshitha']
+
+#printing elements in reversed order
+for o in reversed(system):
+    print(o)
---
+++ Week_12/Day_79/Reverse( )_Parameter.py
@@ -0,0 +1,9 @@
+#operting System list
+system=['Windows','Macos','Linux']
+print('Original List :',system)
+
+#list reverse
+system.reverse()
+
+#updated list
+print('updated list :',system)

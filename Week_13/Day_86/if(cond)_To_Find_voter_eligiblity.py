9c9d8ee2f7c630c4baf1a3cf3f751f22dc1c00b4
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-25 08:42:18 +0530

    Add files via upload

---
+++ Week_13/Day_86/if(cond)_To_Find_voter_eligiblity.py
@@ -0,0 +1,11 @@
+# To Find a Voter eligiblity in if condition
+age=int(input('Enter the age'))
+print('Age is :',age)
+if(age>18):
+    print('Eligible to Voter Id')
+    print('InEligible to Voter Id')
+
+
+
+
+9347333696

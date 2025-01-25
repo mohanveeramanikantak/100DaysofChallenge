373dc7ba54a664c9d37ad597dc5e86ac3b1739f4
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-25 08:40:43 +0530

    Add files via upload

---
+++ Week_13/Day_86/Compound_if.py
@@ -0,0 +1,17 @@
+age=int(input('Enter the name'))
+print(' Age is :',age)
+if(age>18):
+{
+    nationality=str(input('Enter the nationality'))
+    print('Nationality is:',nationality)
+    if(nationality=='I')
+    {
+            print('Eligible')
+    else:
+            print('inEligible')
+    }
+    else
+    {
+        print('inEligible')
+    }
+}

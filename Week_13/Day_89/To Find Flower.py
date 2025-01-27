b45bf2a32e6fa161d3d014c974866515d21722bb
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-27 20:26:13 +0530

    Add files via upload

---
+++ Week_13/Day_89/To Find Flower.py
@@ -0,0 +1,14 @@
+#To Find the Flowers
+flower=str(input("Enter the Flower"))
+print(" L-Lily ")
+print(" R-Rose ")
+print(" J-Jasiman ")
+if(flower=='L'):
+    print("Lilly")
+elif(flower=='R'):
+    print("Rose")
+elif(flower=='J'):
+    print("Jasiman")
+else:
+    print("Not Mentioned")
+    

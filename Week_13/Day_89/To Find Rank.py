798aecd6fe8bac209fb0bd4751f79d3f9ef62b54
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-27 20:27:12 +0530

    Add files via upload

---
+++ Week_13/Day_89/To Find Rank.py
@@ -0,0 +1,13 @@
+# To Find Rank
+rank=int(input("Enter the Rank"))
+print("First-1")
+print("Second-2")
+print("third-3")
+if(rank==1):
+    print("First Rank")
+elif(rank==2):
+    print("Second Rank")
+elif(rank==3):
+    print("Third RanK")
+else:
+    print("Only have three Ranks")

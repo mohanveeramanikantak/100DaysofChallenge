57833dfda26a085e979ee2ce1b1e10aa946c57ab
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-27 20:26:50 +0530

    Add files via upload

---
+++ Week_13/Day_89/To Find Months.py
@@ -0,0 +1,44 @@
+# TO find months in if-elif-else
+month=int(input("Enter The Month"))
+print(" January-1 ")
+print(" Febuary-2 ")
+print(" March-3 ")
+print(" April-4 ")
+print(" May-5 ")
+print(" june-6 ")
+print(" July-7 ")
+print(" August-8 ")
+print(" Sectember-9 ")
+print(" octember-10 ")
+print(" November-11 ")
+print(" Decomber-12 ")
+if(month==1):
+    print("January")
+elif(month==2):
+    print("Febuary")
+elif(month==3):
+    print("March")
+elif(month==4):
+    print("April")
+elif(month==5):
+    print("May")
+elif(month==6):
+    print("June")
+elif(month==7):
+     print("July")
+elif(month==8):
+    print("August")
+elif(month==9):
+    print("Sectember")
+elif(month==10):
+    print("Octember")
+elif(month==11):
+    print("November")
+elif(month==12):
+    pfrint("Decomber")
+else:
+    print("Only have 12 Months ")
+    
+
+    
+      

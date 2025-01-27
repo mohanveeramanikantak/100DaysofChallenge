c429e13f66fb726abaf5741e54f50b66d584142a
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-27 20:27:41 +0530

    Add files via upload

---
+++ Week_13/Day_89/To find week days.py
@@ -0,0 +1,27 @@
+# To Find week days
+day=int(input("Enter The Day"))
+print(" 1-Sunday ")
+print(" 2-Monday ")
+print(" 3-Tuesday ")
+print(" 4-Wednesday ")
+print(" 5-Thursday ")
+print(" 6-Friday ")
+print(" 7-Saturday ")
+if(day==1):
+    print("Sunday")
+elif(day==2):
+    print("Monday")
+elif(day==3):
+    print("Tuesday")
+elif(day==4):
+    print("Wednesday")
+elif(day==5):
+    print("Thursday")
+elif(day==6):
+    print("Friday")
+elif(day==7):
+    print("Saturday")
+else:
+    print("Only Seveen Days For Day")
+    
+        

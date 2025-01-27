caa0819c3ff433032abe1e9d3aff499e83a8a57b
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-27 20:25:09 +0530

    Add files via upload

---
+++ Week_13/Day_89/To Find Color.py
@@ -0,0 +1,24 @@
+# To Find color
+color=str(input(" Enter The Color "))
+print(" B-Black ")
+print(" G-Green ")
+print(" Y-Yellow ")
+print(" V-Volet ")
+print(" W-white ")
+print(" Bl=Blue ")
+if(color=='B'):
+    print("Black")
+elif(color=='G'):
+    print("Green")
+elif(color=='Y'):
+    print("Yellow")
+elif(color=='V'):
+    print("Volet")
+elif(color=='W'):
+    print("White")
+elif(color=='Bl'):
+    print("Blue")
+else:
+    print("Not Mentioned")
+    
+    

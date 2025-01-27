1b12e2865ffc3bed5cc59a6e3dd1a12ae951ec98
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-27 20:25:43 +0530

    Add files via upload

---
+++ Week_13/Day_89/To Find First Second Third int.py
@@ -0,0 +1,13 @@
+# To Find First Second Third
+marks=int(input("Enter the Marks"))
+print("First-70")
+print("Second-50")
+print("third-35")
+if(marks>=70):
+    print("First Rank")
+elif(marks>=50):
+    print("Second Rank")
+elif(marks>=35):
+    print("Third RanK")
+else:
+    print("Only have three Ranks")

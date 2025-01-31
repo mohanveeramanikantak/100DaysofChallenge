4d6d9910aa1a5ba794bc121f1e8d66f2b91e08bc
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-01-31 19:50:49 +0530

    Add files via upload

---
+++ Week_14/Day_91/dictionary from the keyword and print the sum of values.py
@@ -0,0 +1,7 @@
+#Write a program to take distionary from the keyboard and print the sum of values ?
+d=eval(input("Enter dictionary"))
+s=sum(d.values())
+print("sum=",s)
+
+
+#Enter dictionary{'A':100,'B':200,'c':300}
